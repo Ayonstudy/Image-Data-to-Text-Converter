ea7ec194686bada07cccfd5e662932a2e2adf728
Ayonstudy <154357953+Ayonstudy@users.noreply.github.com>
2024-08-26 17:38:26 +0600

    Update IDTC.py

--- Version 1/IDTC.py
+++ Version 1/IDTC.py
@@ -10,33 +10,33 @@ from PIL import Image
 # The first line in the image-data.txt file will be width and hieght respectively
 # From the next line and onwards will be the pixel data in hexadecimal. Each two digit goes in order red,green,blue then finally transperancy level
 # Each pixel data will be seperated by a comma (,) and each new line represents the next row of pixels from the image.
-im = Image.open(sys.argv[1])                                               # sys.argv[1] is parameter for image file to load onto program
+im = Image.open(sys.argv[1])                                                                      # sys.argv[1] is parameter for image file to load onto program
 px = im.load()
-width, hieght = im.size                                                    # get height and width values
+width, hieght = im.size                                                                           # get height and width values
 print()
 
-f = open("image-data.txt", "w")                                                  # to reset previous file
-f.write(str(width) + "," + str(hieght) + "\n")                             # save width and hieght data on first line
+f = open("image-data.txt", "w")                                                                   # to reset previous file
+f.write(str(width) + "," + str(hieght) + "\n")                                                    # save width and hieght data on first line
 f.close()
 progress=0
 for y in range(hieght):
     for x in range(width):
-        try:                                                               # in case t doesn't exist in image file
-            r,g,b,t = px[x,y]                                              # r = red, g = green, b = blue, t = transparency
+        try:                                                                                      # in case t doesn't exist in image file
+            r,g,b,t = px[x,y]                                                                     # r = red, g = green, b = blue, t = transparency
         except ValueError:
-            r,g,b = px[x,y]                                                # r = red, g = green, b = blue
-            t = 255                                                        # t = transparency (set to 0xff)
+            r,g,b = px[x,y]                                                                       # r = red, g = green, b = blue
+            t = 255                                                                               # t = transparency (set to 0xff)
         f = open("image-data.txt", "a")
         if x > 0:
             f.write(",")
         f.write("0x" + format(r, '02x') + format(g, '02x') + format(b, '02x') + format(t, '02x')) # save pixel data from second line
         f.close()
     f = open("image-data.txt", "a")
-    f.write("\n")                                                          # start new line
+    f.write("\n")                                                                                 # start new line
     f.close()
     progress=format(((y/hieght)*100), ".2f")      
-    sys.stdout.write("\rFile Formating Progress: " + str(progress) + "%")  # update and show current progress
+    sys.stdout.write("\rFile Formating Progress: " + str(progress) + "%")                         # update and show current progress
     sys.stdout.flush()
 sys.stdout.write("\r\rFile Formating Progress: 100.00%\n")
-print("Process completed!!")                                               # final result with data will be in file.txt file after completion
+print("Process completed!!")                                                                      # final result with data will be in file.txt file after completion
 print("Data saved in image-data.txt")
