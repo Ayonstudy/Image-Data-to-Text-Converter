237b304930368a2b525b0dc4915752789624130a
Ayonstudy <154357953+Ayonstudy@users.noreply.github.com>
2024-08-26 22:04:10 +0600

    Update IDTC.py

--- Version 1/IDTC.py
+++ Version 1/IDTC.py
@@ -3,19 +3,31 @@ from PIL import Image
 # Image-Data-to-Text-Convertor (IDTC)
 # Made in 20 August 2024 by Ayonstudy :)
 # This program converts Image pixel data into text form. More specifically, it just makes a seperate text file with data about the image.
-# The program requires one parameter --> The name of the image file (e.g. image.png)
-# Example of running command         --> "IDTC image.png" or "./IDTC.py image.png"
-# The image file has to be in the same directory as the IDTC.py file.
-# After running the program, the data will be saved onto the image-data.txt file which will be created after the process is finished.
-# The first line in the image-data.txt file will be width and hieght respectively
+# The program has two parameters
+# The first arguement is compulsory, it is the name of the image file (e.g. image.png)
+# The second arguement is optional, it is the name of the final file to be given where the data will be saved to (e.g. file.txt) [the default is "image-data.txt"]
+# Example of running command         --> "IDTC image.png file.txt" or "./IDTC.py image.png"
+# After running the program, the data will be saved onto the image-data.txt file or the file name you will give as the second arguement.
+# The first line in the text file will be width and hieght respectively
 # From the next line and onwards will be the pixel data in hexadecimal. Each two digit of the hexadecimal values goes in order red,green,blue then finally transperancy level
 # Each pixel data will be seperated by a comma (,) and each new line represents the next row of pixels from the image.
-im = Image.open(sys.argv[1])                                                                      # sys.argv[1] is parameter for image file to load onto program and be converted
+parameter_number = len(sys.argv)                                                                 # Number of arguements
+if parameter_number == 1:                                                                        # Lack of arguements
+    print("Syntax error: Must include image file name")
+    exit()
+elif parameter_number == 2:                                                                      # arguement for image file name
+    final_file_name = "image-data.txt"
+elif parameter_number == 3:                                                                      # arguement for text file name (optional)
+    final_file_name = sys.argv[2]
+elif parameter_number >= 4:                                                                      # Excess arguements
+    print("Syntax error: Too many arguements!")
+    exit()
+im = Image.open(sys.argv[1])                                                                      # sys.argv[1] is parameter for image file to be load onto program and be converted
 px = im.load()
 width, hieght = im.size                                                                           # get height and width values
 print()
 
-f = open("image-data.txt", "w")                                                                   # Empty previous file(change the previous image-data.txt file's name into something else to prevent overwrite)
+f = open(final_file_name, "w")                                                                    # Empty previous file
 f.write(str(width) + "," + str(hieght) + "\n")                                                    # save width and hieght data on first line
 f.close()
 progress=0
@@ -26,17 +38,17 @@ for y in range(hieght):
         except ValueError:
             r,g,b = px[x,y]                                                                       # r = red, g = green, b = blue
             t = 255                                                                               # t = transparency (set to 0xff)
-        f = open("image-data.txt", "a")
+        f = open(final_file_name, "a")
         if x > 0:
             f.write(",")
         f.write("0x" + format(r, '02x') + format(g, '02x') + format(b, '02x') + format(t, '02x')) # save pixel data from second line
         f.close()
-    f = open("image-data.txt", "a")
+    f = open(final_file_name, "a")
     f.write("\n")                                                                                 # start new line
     f.close()
     progress=format(((y/hieght)*100), ".2f")      
     sys.stdout.write("\rFile Formating Progress: " + str(progress) + "%")                         # update and show current progress
     sys.stdout.flush()
 sys.stdout.write("\r\rFile Formating Progress: 100.00%\n")
-print("Process completed!!")                                                                      # final result with data will be in file.txt file after completion
-print("Data saved in image-data.txt")                                                             # recommended to change the filename to something else or store in some directory without the IDTC.py file to prevent overwrite
+print("Process completed!!")                                                                      # final result with data will be in the text file after completion
+print("Data saved in " + final_file_name)                                                         # recommended to use another text file name to prevent overwrite
